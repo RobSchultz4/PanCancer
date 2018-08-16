cfdc86e65ba785f8f3dace2361bf8a164b818088
RobSchultz4 <rschult4@asu.edu>
2018-08-16 12:43:28 -0700

    I built a function to make the if else structure more readable. Also changed the structure to do the analysis for both pfs and os is possible

--- RDP_Surv.py
+++ RDP_Surv.py
@@ -11,43 +11,51 @@ import csv
 import pandas as pd
 import numpy as np
 
-
-
 def runcph(coxdf, dur, eve):
     coxdf = pd.DataFrame(coxdf).T
     cph = CoxPHFitter()
     cph.fit(coxdf, duration_col = dur, event_col = eve, show_progress=True)  
-    return cph.print_summary()
+    return cph
 
+def isincluded(p1, colname):
+    return any(pd.isnull(p1[colname]).apply(lambda x: not x))
 
 p1 = pd.read_csv('C:/Users/rschult4/Dropbox (ASU)/PanCancer/Reconstructed pData/GSE49278_pData_recon.csv',header = 0,index_col = 0)
 coxdf = []
 
-
 # Run if there is any survival info availible
-if any(pd.isnull(p1['OS.STATUS']).apply(lambda x: not x)) and any(pd.isnull(p1['OS.STATUS']).apply(lambda x: not x)) or any(pd.isnull(p1['OS.STATUS']).apply(lambda x: not x)) and any(pd.isnull(p1['OS.STATUS']).apply(lambda x: not x)):
+if isincluded(p1, 'OS.STATUS') and isincluded(p1, 'OS.TIME']) or isincluded(p1, 'PFS.STATUS') and isincluded(p1, 'PFS.TIME'):
     # Run if the info availible is Overall Survival
-    if any(pd.isnull(p1['OS.STATUS']).apply(lambda x: not x)) and any(pd.isnull(p1['OS.STATUS']).apply(lambda x: not x)):
+    if isincluded(p1, 'OS.STATUS') and isincluded(p1, 'OS.TIME']):
         coxdf.append(p1['OS.STATUS'])
         coxdf.append(p1['OS.TIME'])
         dur = 'OS.TIME'
         eve = 'OS.STATUS' 
         surv=runcph(coxdf, dur, eve)
-    # If there is no Overall Survival, use Progression Free Survival
-    else:
+        # Run if there is also age information
+        if isincluded(p1, 'AGE'):
+            coxdf.append(p1['AGE'])
+            survA=runcph(coxdf, dur, eve)
+            # Run if there is also sex information
+            if isincluded(p1, 'SEX'):
+                coxdf.append(pd.get_dummies(p1['SEX'])['M']) # Male is 1 and female is 0
+                survAS=runcph(coxdf, dur, eve)
+            
+    # Run if there is Progression Free Survival
+    if isincluded(p1, 'PFS.STATUS') and isincluded(p1, 'PFS.TIME'):
         coxdf.append(p1['PFS.STATUS'])
         coxdf.append(p1['PFS.TIME'])
         dur = 'PFS.TIME'
         eve = 'PFS.STATUS'
         surv=runcph(coxdf, dur, eve)
-    # Run if there is also age information
-    if any(pd.isnull(p1['AGE']).apply(lambda x: not x)):
-        coxdf.append(p1['AGE'])
-        survA=runcph(coxdf, dur, eve)
-        # Run if there is also sex information
-        if any(pd.isnull(p1['SEX']).apply(lambda x: not x)):
-            coxdf.append(pd.get_dummies(p1['SEX'])['M']) # Male is 1 and female is 0
-            survAS=runcph(coxdf, dur, eve)
+        # Run if there is also age information
+        if isincluded(p1, 'AGE'):
+            coxdf.append(p1['AGE'])
+            survA=runcph(coxdf, dur, eve)
+            # Run if there is also sex information
+            if isincluded(p1, 'SEX'):
+                coxdf.append(pd.get_dummies(p1['SEX'])['M']) # Male is 1 and female is 0
+                survAS=runcph(coxdf, dur, eve)
 else:
     print("No Survival!")
     
